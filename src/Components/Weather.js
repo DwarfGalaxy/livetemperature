3765cee2caf938b6365dd4083d8baa368eba9e16
Yubraj <nontoxicyubs@gmail.com>
2022-08-15 20:30:51 +0545

    first commit

---
+++ src/Components/Weather.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+
+function Weather() {
+    const [cityName, setCityName] = useState("");
+    const [temp, setTemp] = useState();
+    const [minTemp, setMinTemp] = useState();
+    const [maxTemp, setMaxTemp] = useState();
+    const [searchCity, setSearchCity] = useState("");
+    let setCityNames = () => {
+        setCityName(searchCity);
+    }
+    useEffect(() => {
+        if (cityName !== "") {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=0a2955a001a3fd0bfff2b535e3ec07ba`).then((apidata) => {
+                return apidata.json();
+
+            }).then((actualdata) => {
+                setCityName(cityName);
+                // console.log(actualdata.main);
+                // console.log(actualdata.main.temp);
+                setTemp(actualdata.main.temp);
+                setMinTemp(actualdata.main.temp_min);
+                setMaxTemp(actualdata.main.temp_min)
+            }).catch(() => {
+                console.log("Error");
+            });
+        }
+    })
+
+    return (
+        <>
+            <div className="card w-50 border border-0 rounded mx-auto mt-3" id='weatherContainer'>
+                <input type="text" className="form-control w-50 mx-auto border border-dark rounded-pill mt-5" placeholder='search' value={searchCity} onChange={(event) => setSearchCity(event.target.value)} onBlur={() => setCityNames()} />
+                {cityName === "" ? <div className="container fs-1 fw-bolder text-center mt-5">No Data Found</div> :
+                    <>
+                        <div className=" d-flex mt-5 mx-auto">
+                            <i className="fa fa-cloud text-light fs-1 my-auto"></i>
+                            <p className='fs-1 fw-bold ms-3 my-auto' style={{ textTransform: "capitalize" }}>{cityName}</p>
+                        </div>
+                        <p className='fs-1 fw-bolder mx-auto mt-4'>{temp}<sup>0</sup>Cel</p>
+                        <p className='mx-auto'>Min : {minTemp} | Max : {maxTemp}</p>
+
+                    </>}
+              
+                  <img src="weather.gif"className='img-fluid border  border-0 ' alt="" />
+            </div>
+        </>
+    )
+}
+
+export default Weather
